0cc6112004eb05880bea61c67ceb310daf43fb7b
ElJuanxi <jbobadillamorales@gmail.com>
2024-11-15 01:12:01 -0500

    otro porque no deployo

--- src/app/Pages/registro/registro.component.ts
+++ src/app/Pages/registro/registro.component.ts
@@ -95,7 +95,7 @@ export class RegistroListaComponent {
   ver_detalle(id: number) {
     this.id = id;
     this.detalleEdit = this.detalles.find(detalles => this.detalle.factura.idFactura === id)
-    console.log(this.id);
+    console.log("detalle",this.detalleEdit);
     return id;
   }
 
